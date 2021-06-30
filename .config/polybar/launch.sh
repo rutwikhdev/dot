221a7f57a564d8632ae406ea0c802c4f9f72603d
rutwikhdev <rutwikhdev@gmail.com>
2021-07-01 00:03:19 +0530

    gnome theme and icon

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,13 +2,13 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
+# If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar monitor1 &
-# polybar monitor2 &
+polybar monitor2 &
 #polybar top-panel >>/tmp/polybar2.log 2>&1 & disown
 
 echo "Bars launched..."
